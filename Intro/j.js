8509f88bd69664856807a69e38ed40e9046bfaa0
ArpedXD <152394507+ArpedXD@users.noreply.github.com>
2023-11-29 19:56:53 +0800

    Add files via upload

---
+++ Intro/j.js
@@ -0,0 +1,71 @@
+let body = document.getElementById('body');
+let account = document.getElementById('account');
+let fname = document.getElementById('fname');
+let lname = document.getElementById('lname');
+let st = document.getElementById('st');
+let age = document.getElementById('ag');
+let checker = document.getElementById('checker');
+
+function wait() {
+    return new Promise((resolve) => {
+    setTimeout(resolve,1000)})
+    };
+
+    async function onstart(){
+        await wait();
+        await wait();
+        await wait();
+        await wait();
+        body.style.backgroundColor = 'transparent';
+        body.style.backgroundImage = "url('bg.jpg')";
+        await wait();
+        await wait();
+        account.style.top = '50px';
+        await wait();
+        await wait();
+        account.style.right = '600px';
+    }
+
+
+    onstart();
+
+async function check(){
+  let verif1 = '';
+  let verif2 = '';
+
+    if(st.value === 'Student' || st.value === 'student'){
+        verif1 = 'true'
+    }else if(st.value === 'Teacher' || st.value === 'teacher'){
+        verif1 = 'true'
+    }else{
+        verif1 = 'false';
+    }
+
+    if(fname.value === '' || lname.value === '' || st.value === '' || age.value === ''){
+        verif2 = 'false';
+    } else{
+        verif2 = 'true';
+    }
+
+    if(verif2 === 'false'){
+        checker.innerHTML = 'One of them is blank.';
+    } else if(verif1 === 'false'){
+        checker.innerHTML = 'Student Or Teacher Only.';
+    } else{
+        checker.innerHTML = 'Great!';
+    }
+
+    if(verif1 === 'true' && verif2 === 'true'){
+        await wait();
+        checker.innerHTML = 'wait.';
+        await wait();
+        checker.innerHTML += '.';
+        await wait();
+        checker.innerHTML += '.';
+        await wait();
+        checker.innerHTML = '';
+        await wait();
+        account.style.top = '-600px';
+    }
+}
+
---
+++ j.js
@@ -0,0 +1,75 @@
+let maxx = document.getElementById('run');
+let ua = 0;
+let body = document.getElementById('body');
+let code = document.getElementById('Code');
+let welcome = document.getElementById('welcome');
+let start = document.getElementById('start');
+let restart = document.getElementById('restart');
+
+function wait() {
+    return new Promise((resolve) => {
+    setTimeout(resolve,1000)})
+    };
+    function wait2() {
+        return new Promise((resolve) => {
+        setTimeout(resolve,500)})
+        };
+
+
+async function run(){
+    maxx.innerHTML = 'Wait.';
+    await wait();
+    maxx.innerHTML = 'Wait..';
+    await wait();
+    maxx.innerHTML = 'Wait...';
+    await wait();
+    maxx.innerHTML = 'Complete!';
+    await wait();
+    maxx.style.top = '-200px';
+    await wait();
+    maxx.innerHTML = 'Max';
+    await wait2();
+    code.innerHTML = 'Ru';
+    await wait2();
+    code.innerHTML += 'nni';
+    await wait2();
+    code.innerHTML += 'ng';
+    await wait2();
+    code.innerHTML += '.';
+    await wait2();
+    code.innerHTML += '.';
+    await wait2();
+    code.innerHTML += '.';
+    await wait2();
+    await wait2();
+    await wait2();
+    code.innerHTML = '';
+    await wait();
+    welcome.style.opacity = '1';
+    await wait2()
+    welcome.style.top = '200px';
+    await wait();
+    start.style.opacity = '1';
+    restart.style.opacity = '1';
+    await wait2();
+    start.style.top = '220px';
+    start.style.cursor = 'pointer';
+    restart.style.top = '230px';
+    restart.style.cursor = 'pointer';
+}
+
+async function start2(){
+    start.style.transition = '3s';
+    start.style.fontSize = '50px';
+    await wait2();
+    start.style.fontSize = '40px';
+    await wait();
+    await wait();
+    welcome.style.opacity = '0';
+    start.style.transition = '2s';
+    start.style.opacity = '0';
+    restart.style.opacity = '0';
+    await wait();
+    await wait();
+    location.href = 'intro/h.html';
+}
